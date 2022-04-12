925b69520bf98b8687f8a8a5def59cd55e0ed8a2
boomafromboomiland <lubomira.kovacova@avon.com>
2022-04-12 18:31:54 +0200

    prompt implemented

---
+++ prompt.js
@@ -0,0 +1,7 @@
+//vytvorenie mena, priezviska, veku a naplnenie do premenných
+let celeMeno = prompt('Zadaj meno a priezvisko');
+let vek = prompt('Zadaj vek', 18);
+let mestoNarodenia = prompt('Zadaj mesto narodenia ');
+
+//vypísanie výsledkov pomocou alert
+alert(`Voláš sa ${celeMeno} a je ti ${vek} rokov.`);
