9e3e218ad701ab8e39946efb63a615fb8b0c6915
boomafromboomiland <lubomira.kovacova@avon.com>
2022-04-12 19:05:49 +0200

    refactoring

--- input.js
+++ input.js
@@ -2,15 +2,22 @@
 function vypocitajBMI() {
   let vyskaMetry = parseFloat(document.querySelector('input[name=vyska]').value / 100); //z tohto dostaneme textový reťazec --> pridáme parseFloat
   let vahaKilogramy = parseFloat(document.querySelector('input[name=vaha]').value); //z tohto dostaneme textový reťazec --> pridáme parseFloat
-  let BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2)); //výpočet BMI, nič sa nestane, nemá čo funckiu zavolať
-  vypisBMI(BMI);
+  return BMI = Math.round(vahaKilogramy / Math.pow(vyskaMetry, 2)); //výpočet BMI, nič sa nestane, nemá čo funckiu zavolať
+  //vypisBMI(BMI);
 };
 
 // funckia pre zapísanie BMI do User Interface (UI)
 function vypisBMI(BMI) {
   document.querySelector('#vysledok > span').innerHTML = BMI;
 };
+
+// refactoring výpisu BMI
+function vypocitajVypisBMI() {
+  let BMI = vypocitajBMI();
+  vypisBMI(BMI);
+};
+
 // zavolanie výpočet BMI
 document.querySelectorAll('input').forEach((element) => {
-  element.addEventListener('change', vypocitajBMI);
+  element.addEventListener('change', vypocitajVypisBMI);
 });
